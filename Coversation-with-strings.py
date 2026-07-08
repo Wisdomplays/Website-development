00209a5e13056594011d649aa4b709129e2ad277
Wisdomplays <Chitmawa637@gmail.com>
2026-07-08 19:04:47 +0700

    Add files via upload
    In this project, you have to store projects

---
+++ Coversation-with-strings.py
@@ -0,0 +1,7 @@
+sentence1 = input("Enter the first sentence")
+sentence2 = input("Enter the second sentence")
+sentence3 = input("Enter the third sentence")
+
+result= sentence1 + " " + sentence2 + " " + sentence3
+
+print("\nConcatenated Sentence:")
